d075863d317bd42015de544839d65ccdc70699ac
Aeysha Ibrahim Mahmoud <ayoosh0056@gmail.com>
2019-06-20 16:50:45 +0300

    adding authentication & authorization

--- app/Http/Controllers/ProjectsController.php
+++ app/Http/Controllers/ProjectsController.php
@@ -8,9 +8,15 @@ use App\Project;
 class ProjectsController extends Controller
 {
     //
+
+    public function __construct()
+    {
+        $this->middleware('auth');
+    }
+
     public function index()
     {
-        $projects = Project::all();
+        $projects = Project::where('owner_id', auth()->id())->get();
 
         return view('projects.index', compact('projects'));
 
@@ -25,12 +31,15 @@ class ProjectsController extends Controller
 
     public function show(Project $project)
     {
+        abort_if($project->owner_id !== auth()->id(), 403); 
 
         return view('projects.show', compact('project'));
 
     }
     public function update(Project $project)
     {
+        abort_if($project->owner_id !== auth()->id(), 403);
+
         $project->update(request(['title', 'description']));
 
         return redirect('/projects');
@@ -38,12 +47,15 @@ class ProjectsController extends Controller
     }
     public function edit(Project $project) 
     {
+        abort_if($project->owner_id !== auth()->id(), 403);
 
         return view('projects.edit', compact('project'));
 
     }
     public function destroy(Project $project)
     {
+        abort_if($project->owner_id !== auth()->id(), 403);
+
          $project->delete();
 
         return redirect('/projects');
@@ -56,6 +68,9 @@ class ProjectsController extends Controller
             'title' => ['required','min:3'],
             'description' => ['required','min:3']
         ]);
+
+        $attributes['owner_id'] = auth()->id();
+
         Project::create($attributes);
 
         return redirect('/projects');
--- routes/web.php
+++ routes/web.php
@@ -24,3 +24,7 @@ Route::post('/projects/{project}/tasks', 'ProjectTasksController@store');
 Route::patch('/tasks/{task}', 'ProjectTasksController@update');
 
 
+
+Auth::routes();
+
+Route::get('/home', 'HomeController@index')->name('home');
